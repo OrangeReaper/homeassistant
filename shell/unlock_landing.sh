5d6a466a842c0521d3f5a08e0178a56c1a01db6d
Andy Bond <andy.bond@zoho.com>
2019-03-30 14:40:28 +0000

    Lovelace & Porch PIR

--- shell/unlock_landing.sh
+++ shell/unlock_landing.sh
@@ -2,7 +2,7 @@
 source /home/homeassistant/.homeassistant/shell/ipAddresses.sh
 #Unlock Landing
 echo -n "004,\!R3D6Fu" | nc -u -w1 $lightwaverf 9760
-sleep 2
+sleep 20
 echo -n "004,\!R3D6Fu" | nc -u -w1 $lightwaverf 9760
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 $path/logevent.sh "Landing Unlocked"
